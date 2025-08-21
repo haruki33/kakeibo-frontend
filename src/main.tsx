5a02b663b11183c8d15eb65398c8523a5e61a293
haruki <kkhk0869hs33@gmail.com>
2025-08-21 13:44:58 +0000

    常にlightmode

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "@/components/ui/provider";
+import { LightMode } from "./components/ui/color-mode.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <LightMode>
+        <App />
+      </LightMode>
     </Provider>
   </StrictMode>
 );
