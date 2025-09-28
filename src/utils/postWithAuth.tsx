f539b7bf03b688956b9799deadac8bb0b4cb2d3c
haruki <kkhk0869hs33@gmail.com>
2025-09-28 14:42:16 +0900

    エラー修正

--- src/utils/postWithAuth.tsx
+++ src/utils/postWithAuth.tsx
@@ -1,9 +1,9 @@
-import type { AddCategory } from "../types/mysetting.ts";
+import type { Category } from "../types/mysetting.ts";
 import type { Transaction } from "../types/myregister.ts";
 
 export const postWithAuth = async (
   url: string,
-  postData: AddCategory | Transaction
+  postData: Category | Transaction
 ) => {
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
   let accessToken = localStorage.getItem("accessToken");
