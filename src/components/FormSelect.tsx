42c60a39a54fc0f85d20960fd3b9ba1dcbeb4a0d
haruki <kkhk0869hs33@gmail.com>
2025-11-29 11:06:47 +0900

    Chakra UIを使用した再利用可能なフォーム選択コンポーネントを追加
    react-hook-formとChakra UIを統合した汎用的な選択入力要素を導入し、検証機能をサポートした一貫性のあるドロップダウン選択を実現します。共通の選択ロジックを抽象化することで、フォームの再利用性と保守性を向上させます。

---
+++ src/components/FormSelect.tsx
@@ -0,0 +1,57 @@
+import { Select, type ListCollection } from "@chakra-ui/react";
+import type { Category, typeSelect } from "../types/mysetting";
+import { Controller, type Control } from "react-hook-form";
+
+type formSelectProps = {
+  name: keyof Category;
+  control: Control<Category>;
+  collections: ListCollection<typeSelect>;
+  label: string;
+  placeholder: string;
+};
+
+function FormSelect({
+  name,
+  control,
+  collections,
+  label,
+  placeholder,
+}: formSelectProps) {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <Select.Root
+          collection={collections}
+          name={field.name}
+          value={[String(field.value)]}
+          onValueChange={(e) => field.onChange(e.value[0])}
+        >
+          <Select.HiddenSelect />
+          <Select.Label>{label}</Select.Label>
+          <Select.Control>
+            <Select.Trigger>
+              <Select.ValueText placeholder={placeholder} />
+            </Select.Trigger>
+            <Select.IndicatorGroup>
+              <Select.Indicator />
+            </Select.IndicatorGroup>
+          </Select.Control>
+          <Select.Positioner>
+            <Select.Content>
+              {collections.items.map((collection) => (
+                <Select.Item item={collection} key={collection.value}>
+                  {collection.label}
+                  <Select.ItemIndicator />
+                </Select.Item>
+              ))}
+            </Select.Content>
+          </Select.Positioner>
+        </Select.Root>
+      )}
+    />
+  );
+}
+
+export default FormSelect;
