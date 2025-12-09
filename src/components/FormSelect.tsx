19bc6d86da2eea575f4cd60fe8ee47e1525fd347
haruki <kkhk0869hs33@gmail.com>
2025-12-09 23:37:02 +0900

    型定義を修正し、FormSelectコンポーネントのカテゴリ型をCategoryTypeに更新しました。

--- src/components/FormSelect.tsx
+++ src/components/FormSelect.tsx
@@ -1,10 +1,10 @@
 import { Select, type ListCollection } from "@chakra-ui/react";
-import type { Category, typeSelect } from "../types/mysetting";
+import type { CategoryType, typeSelect } from "../types/mysetting";
 import { Controller, type Control } from "react-hook-form";
 
 type formSelectProps = {
-  name: keyof Category;
-  control: Control<Category>;
+  name: keyof CategoryType;
+  control: Control<CategoryType>;
   collections: ListCollection<typeSelect>;
   label: string;
   placeholder: string;
